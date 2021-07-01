fc8c357cedf557fd095780ab5b0455d4cc9dcf33
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-07-01 17:56:37 +0600

    end of variable

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Sam");
+		
+		System.out.println(3+10);
+		System.out.println(10-7);
+		System.out.println(10*7);
+		System.out.println(10/7);
+	}
+
+}
---
+++ src/Variable.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int constantNumber = 20;
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
+		
+		System.out.println(constantNumber +number1);
+		System.out.println(constantNumber*number2);
+		
+		float number3 = 4.53f;
+		double number4 = 5.23;
+		
+		double number5 = scanner.nextDouble();
+		float number6 = scanner.nextFloat();
+		
+		
+		int temp= 10;
+		
+		int for =40;
+		
+		
+		
+	}
+
+}
